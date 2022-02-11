ccb57c20ba570833e0dd37c7c283d673e8640d04
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-02-11 15:40:16 +0200

    some rewritings in my directive with HostBinding

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -16,12 +16,18 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseover', ['$event'] ) mouseOver(event: any): void {
     // console.log(event);
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightgreen', );
- }
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightgreen', );
+    this.backgroundColor = 'blue';
+    this.color = 'white';
+  }
 
- @HostListener('mouseleave', ['$event'] ) mouseLeave(event: any): void {
-   this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white', );
- }
+  @HostListener('mouseleave', ['$event'] ) mouseLeave(event: any): void {
+   //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white', );
+   this.backgroundColor = 'white';
+   this.color = 'red';
+  }
 
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'white';
+  @HostBinding('style.color') color: string = 'red';
 
 }
